d7a32f379efe8c633399278e47506fdb418ad0ae
dzyanino <dzyanino@gmail.com>
2026-03-30 12:26:14 +0300

    Fix : logical error about position and not distance

--- scripts/particle.gd
+++ scripts/particle.gd
@@ -1,30 +1,35 @@
+class_name Particle
 extends CharacterBody2D
 
-@export var max_speed: float = 20.0
+@export var max_speed: float = 200.0
 @export var wander_strength: float = 5.0
 @export var change_direction_every: float = 0.1
 
 var wander_timer: float = 0.0
 
+var objective: Vector2 = Vector2.ZERO
+var best_distance: float = INF
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
-
+		
 	if velocity.length() > max_speed:
 		velocity = velocity.limit_length(max_speed)
-	
-	wander_timer -= delta
-	if wander_timer <= 0.0:
-		var random_nudge = Vector2.from_angle(randf() * TAU) * wander_strength
-		velocity += random_nudge
-		wander_timer = change_direction_every
-	
-	var viewport = get_viewport_rect()
-	if position.x < 0:
-		position.x = viewport.size.x
-	elif position.x > viewport.size.x:
-		position.x = 0
 		
-	if position.y < 0:
-		position.y = viewport.size.y
-	elif position.y > viewport.size.y:
-		position.y = 0
+	if (not get_tree().has_group("target")):
+		wander_timer -= delta
+		if wander_timer <= 0.0:
+			var random_nudge = Vector2.from_angle(randf() * TAU) * wander_strength
+			velocity += random_nudge
+			wander_timer = change_direction_every
+		
+		var viewport = get_viewport_rect()
+		if position.x < 0:
+			position.x = viewport.size.x
+		elif position.x > viewport.size.x:
+			position.x = 0
+			
+		if position.y < 0:
+			position.y = viewport.size.y
+		elif position.y > viewport.size.y:
+			position.y = 0
