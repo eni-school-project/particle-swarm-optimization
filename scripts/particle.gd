4046d40f46cd153f158b338a678c7894dae3f753
dzyanino <dzyanino@gmail.com>
2026-04-01 23:48:48 +0300

    Feat : add particle deceleration before wandering again and refactored the `_physics_process` function

--- scripts/particle.gd
+++ scripts/particle.gd
@@ -4,33 +4,47 @@ extends CharacterBody2D
 @export var max_speed: float = 20.0
 @export var wander_strength: float = 15.0
 @export var change_direction_every: float = 0.1
+@export var deceleration: float = 0.92
 
 var wander_timer: float = 0.0
 
 var has_target: bool = false
+var is_decelerating: bool = false
+
 var objective: Vector2 = Vector2.ZERO
 var best_distance: float = INF
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
-	if (velocity.length() > max_speed):
-		velocity = velocity.limit_length(max_speed)
-
-	if not has_target:
-		wander_timer -= delta
-		if (wander_timer <= 0.0):
-			var random_nudge = Vector2.from_angle(randf() * TAU) * wander_strength
-			velocity += random_nudge
-			wander_timer = change_direction_every
+	if is_decelerating:
+		velocity *= pow(deceleration, delta * 60.0)
+		if (velocity.length() <= 20.0):
+			is_decelerating = false
+			max_speed = 20.0
+	
+	else:
+		if (velocity.length() > max_speed):
+			velocity = velocity.limit_length(max_speed)
 		
-		var viewport = get_viewport_rect()
-		if (position.x < 0): position.x = viewport.size.x
-		elif (position.x > viewport.size.x): position.x = 0
-		if (position.y < 0): position.y = viewport.size.y
-		elif (position.y > viewport.size.y): position.y = 0
+		if not has_target:
+			wander_timer -= delta
+			if (wander_timer <= 0.0):
+				var random_nudge = Vector2.from_angle(randf() * TAU) * wander_strength
+				velocity += random_nudge
+				wander_timer = change_direction_every
+			
+			var viewport = get_viewport_rect()
+			if (position.x < 0): position.x = viewport.size.x
+			elif (position.x > viewport.size.x): position.x = 0
+			if (position.y < 0): position.y = viewport.size.y
+			elif (position.y > viewport.size.y): position.y = 0
 
 
 func set_target(value: bool) -> void:
 	has_target = value
-	max_speed = 200.0 if has_target else 20.0
+	if value:
+		is_decelerating = false
+		max_speed = 200.0
+	else:
+		is_decelerating = true
