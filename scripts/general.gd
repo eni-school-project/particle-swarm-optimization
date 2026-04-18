aec86bea8218fb181e2e04d0ae6c74eb8c49c6a1
dzyanino <dzyanino@gmail.com>
2026-04-18 10:14:23 +0300

    Feat : added back to main menu button

--- scripts/general.gd
+++ scripts/general.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_back_pressed() -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://main.tscn")
