50bb6b7fd097ab6995cb27a73b72e1c2db725017
dzyanino <dzyanino@gmail.com>
2026-05-15 21:45:19 +0300

    Feat: added `specific` scene for specific case

---
+++ scripts/specific_right_container.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+
+@export var red_point_number: int = 8
+@export var green_point_number: int = 8
+
+@export var red_point_scene: PackedScene
+@export var green_point_scene: PackedScene
+@export var target_scene: PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
